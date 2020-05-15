7f42a81a1491c65c7e727fd51e149fb82d87a795
Amaury Soto <amaurysotog@gmail.com>
2020-05-15 17:08:18 -0500

    Version 5.1 Scope

--- assets/index.js
+++ assets/index.js
@@ -1,28 +1,7 @@
+import MediaPlayer from "./MediaPlayer.js";
+
 const video = document.querySelector("video");
+const player = new MediaPlayer({ el: video });
+
 const button = document.querySelector("button");
-
-function mediaPlayer(config) {
-  this.media = config.el;
-}
-
-mediaPlayer.prototype.play = function () {
-  this.media.play();
-};
-
-mediaPlayer.prototype.pause = function () {
-  this.media.pause();
-};
-
-/*   const player = new mediaPlayer({ el: video });
-      button.onclick = () => {
-        if (player.media.paused) {
-          player.play();
-        } else {
-          player.pause();
-        }
-      }; */
-
-const player = new mediaPlayer({ el: video });
-button.onclick = () => {
-  player.media.paused ? player.play() : player.pause();
-};
+button.onclick = () => player.togglePlay();
